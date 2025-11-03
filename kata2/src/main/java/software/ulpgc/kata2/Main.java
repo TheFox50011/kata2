a527469b47e264543a4eafc656a978939aa2ffcd
TheFox50011 <saulpablofg2@gmail.com>
2025-11-03 10:32:12 +0000

    Creado la clase main

---
+++ kata2/src/main/java/software/ulpgc/kata2/Main.java
@@ -0,0 +1,18 @@
+package software.ulpgc.kata2;
+
+import software.ulpgc.kata2.io.MovieLoader;
+import software.ulpgc.kata2.io.RemoteMovieLoader;
+import software.ulpgc.kata2.model.Movie;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        MovieLoader movieLoader = new RemoteMovieLoader();
+        List<Movie> movies = movieLoader.loadAll();
+        for(Movie movie: movies){
+            System.out.println(movie);
+        }
+
+    }
+}
